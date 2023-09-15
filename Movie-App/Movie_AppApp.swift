dc0643e719b69fcf6e821a6a02772bc075cac5bd
William Santoso <williamsantoso@keda-tech.com>
2023-09-15 20:50:32 +0700

    Initial Commit

---
+++ Movie-App/Movie_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Movie_AppApp.swift
+//  Movie-App
+//
+//  Created by William Santoso on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
